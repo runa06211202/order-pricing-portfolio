aa222a003c03868c4f4a59b4c3cb1d3c6f17685d
runa06211202 <runa06211202@gmail.com>
2025-10-11 17:18:37 +0900

    割引なしロジック実装、緑確認済
    割引なしロジック実装、Normal系緑確認済(Threshold、Abnomal該当ケース無し)

--- src/main/java/com/example/order/app/OrderService.java
+++ src/main/java/com/example/order/app/OrderService.java
@@ -1,5 +1,12 @@
 package com.example.order.app;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.order.domain.Product;
+import com.example.order.dto.DiscountType;
 import com.example.order.dto.OrderRequest;
 import com.example.order.dto.OrderRequest.Line;
 import com.example.order.dto.OrderResult;
@@ -37,9 +44,34 @@ public class OrderService {
 	  if(req.region() == null || req.region().isBlank()) {
 		  throw new IllegalArgumentException(REGION + "must not be null or blank strings");
 	  }
+	  
+	  BigDecimal subtotalBase = BigDecimal.ZERO;
 	  for(Line line : req.lines()) {
-		  products.findById(line.productId()).orElseThrow(() -> new IllegalArgumentException( "product not found: " + line.productId()));
+		  // Optional<Product>をここでunwrap
+		  Product product = products.findById(line.productId())
+				  .orElseThrow(() -> new IllegalArgumentException( "product not found: " + line.productId()));
+
+		  BigDecimal lineSubtotal = product.unitPrice()
+				  .multiply(BigDecimal.valueOf(line.qty()))
+				  .setScale(2, RoundingMode.HALF_UP);
+
+		  subtotalBase = subtotalBase.add(lineSubtotal);
 	  }
-    throw new UnsupportedOperationException("not implemented yet");
+
+	  BigDecimal totalNetBeforeDiscount = BigDecimal.ZERO;
+	  BigDecimal totalDiscount = BigDecimal.ZERO;
+	  BigDecimal totalTax = BigDecimal.ZERO;
+	  BigDecimal totalGross = BigDecimal.ZERO;
+	  List<DiscountType> appliedDiscounts = new ArrayList<DiscountType>();
+
+	  totalNetBeforeDiscount = subtotalBase;
+	  
+	  //税計算(仮)
+	  totalTax = tax.calcTaxAmount(totalNetBeforeDiscount, "JP", RoundingMode.HALF_UP);
+	  totalGross = tax.addTax(totalNetBeforeDiscount, "JP", RoundingMode.HALF_UP);
+	  
+	  OrderResult orderResult = new OrderResult(totalNetBeforeDiscount, totalDiscount, totalTax, totalGross, appliedDiscounts);
+
+	  return orderResult;
   }
 }
